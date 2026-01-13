7df4cc47b9f8209293ee5898f6c87a2f5c666ad2
Vivek Singh <v.q.kumar.singh@accenture.com>
2026-01-13 11:36:27 +0530

    code changes

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App: React.FC = () => {
 
   useEffect(() => {
     const saved = localStorage.getItem("theme");
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     if (saved === "dark") setDarkMode(true);
   }, []);
 
--- src/global.d.ts
+++
@@ -1,4 +0,0 @@
-// src/global.d.ts
-declare module "*.module.css";
-declare module "*.module.scss";
-declare module "*.css";
